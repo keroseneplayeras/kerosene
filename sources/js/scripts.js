daf6dbd838309c45de13ad3010e74507751bcebf
Santiago <bomsantiago@gmail.com>
2018-05-24 19:51:02 -0500

    e
    e

--- sources/js/scripts.js
+++ sources/js/scripts.js
@@ -1,15 +1,19 @@
-var body_nuevos;
-var body_categorias;
-var body_popular;
-
-$(document).ready(function () {
+    var body_nuevos;
+    var body_categorias;
+    var body_popular;
+    var body_todas;
     var url ="./sources/catalogo.json";
     var jsonData;
+
+$(document).ready(function () {
+
     body_nuevos = "";
     body_categorias = "";
     body_popular = "";
-	//$("#container").load('ejemplo1.html');
 
+    cargarDataJson("param1", "param2", "param3");
+	//$("#container").load('ejemplo1.html');
+/*
     try{
     	$.ajax({
             url: url, // path to file
@@ -31,7 +35,7 @@ $(document).ready(function () {
      } catch ( e ) {
         alert("ERROR: "+e);
      }
-
+*/
    /*$.getJSON(url, function (data) {
         alert(data.miapp.id);
 		$("#container").html(data.miapp.id);	
@@ -79,10 +83,34 @@ $(document).ready(function () {
 
 });
 
+function cargarDataJson(param1, param2, param3) {
+    try{
+        $.ajax({
+            url: url, // path to file
+            type: "GET",
+            async: true,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json", // type of file (text, json, xml, etc)
+            success: function(data) { // callback for successful completion
+                //console.log(data);
+                jsonData=data;
+                //console.log(jsonData);
+                renderizarVistaPrincipal(param1,jsonData);
+                //$("#cargar_nuevo").html(JSON.stringify(data));
+            },
+            error: function() { // callback if there's an error
+              alert("error");
+            }
+        });   
+     } catch ( e ) {
+        alert("ERROR: cargarDataJson "+e);
+     }
+}
+
 /**
 * Armar la vista con el JSON.
 **/
-function renderizarVistaPrincipal(jsonData) {
+function renderizarVistaPrincipal(param1, jsonData) {
     var jsonDataNuevos;
     var jsonDataPopular;
     var jsonDataCategorias;
@@ -106,7 +134,7 @@ function renderizarVistaPrincipal(jsonData) {
                 alert("ERROR: Tamaño de registros "+length);
             }
             if(lengthCategorias > 0){
-                crearVistaCategorias(jsonDataCategorias);
+                crearVistaCategorias(param1, jsonDataCategorias);
             } else{
                 alert("ERROR: Tamaño de registros "+length);
             }
@@ -134,7 +162,7 @@ function crearVistaNuevos(jsonDataNuevos) {
                              +" "+jsonDataNuevos[i].imagen+" 1200w, "
                              +" "+jsonDataNuevos[i].imagen+" 1440w' "
                              +" sizes='(max-width: 400px) 100vw, 400px'>            </a> "
-                            +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' target='_blank' href='#'>Detalle</a>"
+                            +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' href='#'>Detalle</a>"
                            +"</div>"
                            +"<div class='theme-card__footer'>"
                            +"   <div class='theme-card__footer__item'>"
@@ -187,7 +215,7 @@ function crearVistaPopular(jsonDataPopular) {
                  +" "+jsonDataPopular[i].imagen+" 1200w, "
                  +" "+jsonDataPopular[i].imagen+" 1440w' "
                  +" sizes='(max-width: 400px) 100vw, 400px'>            </a> "
-                +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' target='_blank' href='#'>Detalle</a>"
+                +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' href='#'>Detalle</a>"
                +"</div>"
                +"<div class='theme-card__footer'>"
                +"   <div class='theme-card__footer__item'>"
@@ -222,7 +250,7 @@ function crearVistaPopular(jsonDataPopular) {
     $("#cargar_popular").html(body_popular);
 }
 
-function crearVistaCategorias(jsonDataCategorias) {
+function crearVistaCategorias(param1, jsonDataCategorias) {
     console.log("crearVistaCategorias");
     body_categorias = "<ul class='row'>";
     var keysCategorias;
@@ -246,7 +274,7 @@ function crearVistaCategorias(jsonDataCategorias) {
                      +" "+jsonDataCategoria[j].imagen+" 1200w, "
                      +" "+jsonDataCategoria[j].imagen+" 1440w' "
                      +" sizes='(max-width: 400px) 100vw, 400px'>            </a> "
-                    +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' target='_blank' href='#'>Detalle</a>"
+                    +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' href='#'>Detalle</a>"
                    +"</div>"
                    +"<div class='theme-card__footer'>"
                    +"   <div class='theme-card__footer__item'>"
@@ -283,5 +311,63 @@ function crearVistaCategorias(jsonDataCategorias) {
 }
 
 
+function crearVistaPorCategorias(jsonDataCategorias) {
+    console.log("crearVistaCategorias");
+    body_todas = "<ul class='row'>";
+    var keysCategorias;
+    var keyUnit;
+    try{
 
+        for (var i = 0; i < Object.keys(jsonDataCategorias).length; i++) {
+            keysCategorias=Object.keys(jsonDataCategorias);
+            var jsonDataCategoria = jsonDataCategorias[keysCategorias[i]];
+
+            for (var j = 0; j < Object.keys(jsonDataCategoria).length; j++) {
+                
+                body_todas +="<li class='col-md-3 col-6'><div class='theme-card'><div class='theme-card__body'><a class='d-block' href='#'> "
+                +"<img width='400' height='300' src="+jsonDataCategoria[j].imagen+"  "
+                      +"class='theme-card__img wp-post-image' alt='' srcset="+jsonDataCategoria[j].imagen+" 400w, "
+                     +" "+jsonDataCategoria[j].imagen+" 300w, "
+                    +"  "+jsonDataCategoria[j].imagen+" 768w, "
+                     +" "+jsonDataCategoria[j].imagen+" 1024w, "
+                     +" "+jsonDataCategoria[j].imagen+" 200w, "
+                     +" "+jsonDataCategoria[j].imagen+" 600w, "
+                     +" "+jsonDataCategoria[j].imagen+" 1200w, "
+                     +" "+jsonDataCategoria[j].imagen+" 1440w' "
+                     +" sizes='(max-width: 400px) 100vw, 400px'>            </a> "
+                    +"  <a class='theme-card__body__overlay btn btn-brand btn-sm' href='#'>Detalle</a>"
+                   +"</div>"
+                   +"<div class='theme-card__footer'>"
+                   +"   <div class='theme-card__footer__item'>"
+                   +"      <a class='theme-card__title mr-1' href='#'>"+jsonDataCategoria[j].nombre+"</a>"
+                    +"     <p class='theme-card__info'>Tallas"
+                     +"    </p>"
+                      +"   <ul class='prod_cats_list'>"
+                       +"     <li><a href='#'>Colores</a></li>"
+                        +" </ul>"
+                     +"    <p></p>"
+                    +"  </div>"
+                     +" <div class='theme-card__footer__item'>"
+                      +"   <p class='theme-card__price'><span class='woocommerce-Price-amount amount'><span class='woocommerce-Price-currencySymbol'>$</span>"+jsonDataCategoria[j].presio+"</span></p>"
+                    +"     <p class='theme-card__info'>Tallas"
+                     +"    </p>"
+                       +"  <ul class='rating'>"
+                        +"    <li class='rating__item '></li>"
+                         +"   <li class='rating__item '></li>"
+                          +"  <li class='rating__item '></li>"
+                    +"        <li class='rating__item '></li>"
+                     +"       <li class='rating__item '></li>"
+                      +"   </ul>"
+                +"      </div>"
+                +"   </div>"
+               +" </div>"
+            +" </li>";
+            }
+        }
+        body_todas += "</ul>";
+    } catch ( e ) {
+       alert("ERROR: crearVistaCategorias "+e);
+    }
+    $("#cargar_todas").html(body_categorias);
+}
 
